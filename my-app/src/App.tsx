2852043e6c7b20eb14e149bbca60f6519e4b7771
Irina <irenserge@mail.ru>
2022-05-18 14:56:17 +0300

    feat: add settings

---
+++ my-app/src/App.tsx
@@ -0,0 +1,11 @@
+import './main.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Start a project</header>
+    </div>
+  );
+}
+
+export default App;
