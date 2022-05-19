fbc3a6512771113fd948c41e680a3c2499e65de9
Irina <irenserge@mail.ru>
2022-05-19 15:40:56 +0300

    feat: create start page

--- my-app/src/pages/StartPage/StartPage.style.ts
+++ my-app/src/pages/StartPage/StartPage.style.ts
@@ -1,3 +1,30 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  height: 700px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+  font-weight: bold;
+  font-size: 24px;
+`;
+
+export const Image = styled.div`
+  width: 65px;
+  height: 75px;
+  margin-bottom: 45px;
+  img {
+    width: 100%;
+  }
+`;
+
+export const Text = styled.p`
+  font-weight: 400;
+  font-size: 22px;
+  line-height: 140%;
+  font-weight: 400;
+  color: var(--color-text);
+`;
